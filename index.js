28fd331b922e24a35b9dc3152496153da357c7b4
Andrei Neculau <andrei.neculau@gmail.com>
2021-11-18 13:31:12 +0100

    wip

---
+++ index.js
@@ -0,0 +1,8 @@
+// FIXME bootstrap doesn't fire load events e.g. scrollspy. Loading scripts via script tags in html instead
+// import("./node_modules/lodash/lodash.js");
+// import("./node_modules/bootstrap/dist/js/bootstrap.bundle.js");
+
+let {
+  _,
+  bootstrap
+} = this;
